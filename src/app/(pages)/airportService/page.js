55cf28102879b9d740af29397d5cb017462b91eb
Oxayr <uzairmaqsood451@gmail.com>
2025-01-29 14:12:56 +0500

    airport service page completed

---
+++ src/app/(pages)/airportService/page.js
@@ -0,0 +1,41 @@
+/** @format */
+
+import AboutUs from "@/app/_components/AboutUs";
+import Footer from "@/app/_components/Footer";
+import GetQoute from "@/app/_components/GetQoute";
+import HeroSection from "@/app/_components/HeroSection";
+import NavHeader from "@/app/_components/NavHeader";
+import RidePricing from "@/app/_components/RidePricing";
+import React from "react";
+
+function AirportService() {
+  return (
+    <div className=" font-abhaya">
+      <div>
+        <NavHeader />
+        <HeroSection
+          heroTitle="Heathrow Airport Transfers"
+          heroDetails=" With all Heathrow Airport luxury chauffeur transfers punctuality is important to you as a client. "
+          heroBackgroundImage="/airport-hero.svg"
+        />
+      </div>
+      <div className="lg:px-32 px-8  pt-32 bg-black ">
+        <AboutUs
+          aboutUsDetails="You will be met by one of our uniformed professional chauffeurs who will be waiting to welcome you when you step out of the arrivals hall. He will be holding a board with your name on it. You will expect us to be discreet at all times. If you prefer we can arrange a meeting point and phone or text you when you are in the baggage hall."
+          aboutUsHeading="Your Safety Is With Us"
+          buttonText="Contact Us"
+          aboutUsImage="/safety.svg"
+          aboutUsQuestionRequired={false}
+        />
+      </div>
+      <div>
+        <RidePricing />
+
+        <GetQoute />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default AirportService;
